45deaefb8efe5a6be770c4f5ea34be611efbb5e2
BrandonShoemaker <brandon.shoemaker2080@gmail.com>
2021-02-17 19:27:48 -0600

    Added new fucntion and error handling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,30 @@
 var formEl = window.document.querySelector("#task-form");
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
 
-function createTaskHandler(event){
+function taskFormHandler(event){
     // prevent page reload
     event.preventDefault();
     // grabs inputs values
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    if(!taskNameInput || !taskTypeInput){
+        formEl.reset();
+        alert("You must enter a valid value!");
+        return false;
+    }
+    //creates growable object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    }
+    createTaskEl(taskDataObj);
+}
+
+function createTaskEl(taskDataObj){
     // creates reference to element the new task will be appended too=
     var ulEl = document.querySelector("#tasks");
     // creates a reference to the new element to be appended
-    var task = document.createElement("li")
+    var task = document.createElement("li");
     // assigns a class name
     task.className = "task-item";
     // creates a container for contents in the task
@@ -18,7 +32,7 @@ function createTaskHandler(event){
     // assigns class name to the container
     taskInfoEl.className = "task-info";
     // adds HTML to the container
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
     // appends container to end of reference element
     task.appendChild(taskInfoEl);
     ulEl.appendChild(task);
